8a9335a6cc461845a4e2fa249d1aafa985bc9d92
mauiman77 <boerbasher@protonmail.com>
2023-04-29 14:34:22 +0100

    Create mobile button layout

--- module_playground_main.js
+++ module_playground_main.js
@@ -6,6 +6,7 @@ main.appendChild(buttonMain);
 
 let count;
 let incrementMargin = 0;
+let addCurve = 1.05;
 
 for (count = 0; count < 7; count += 1) {
   incrementMargin += 30;
@@ -13,7 +14,8 @@ for (count = 0; count < 7; count += 1) {
   const buttonOption = document.createElement('button');
   buttonOption.classList.add('mobile', 'option');
   buttonOption.textContent = '!';
-  buttonOption.style.marginRight = `${incrementMargin}px`;
-  buttonOption.style.marginBottom = `${incrementMargin * 1.2}px`;
+  buttonOption.style.marginRight = `${incrementMargin * addCurve}px`;
+  buttonOption.style.marginBottom = `${incrementMargin * 1.8}px`;
+  addCurve *= 1.1;
   main.appendChild(buttonOption);
 }
