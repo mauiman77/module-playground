624c007e8af8e5d35ccd1fa741181ae28ea5b644
mauiman77 <boerbasher@protonmail.com>
2023-04-30 12:49:54 +0100

    Add experimental autoslide

--- module_playground_main.js
+++ module_playground_main.js
@@ -1,2 +1,40 @@
-const slider = document.getElementById('#slider');
+const slider = document.getElementById('slider');
+const colours = ['DarkCyan', 'DarkOrchid', 'DarkRed', 'DodgerBlue'];
+const rightButton = document.querySelector('button.right');
+const leftButton = document.querySelector('button.left');
+let imageCount = 0;
+slider.style.backgroundColor = colours[0];
 
+function changeImageCount(button) {
+	if (button === rightButton) {
+		return imageCount > colours.length-2 ? imageCount : imageCount += 1;
+	}
+	else {
+		return imageCount < 1 ? imageCount : imageCount -= 1;
+	}
+}
+
+function changeSlider(number) {
+	return imageCount += number;
+}
+
+rightButton.addEventListener('click', () => {
+	slider.style.backgroundColor = colours[changeImageCount(rightButton)];
+	console.log(imageCount);
+}
+);
+
+leftButton.addEventListener('click', () => {
+	slider.style.backgroundColor = colours[changeImageCount(leftButton)];
+}
+);
+
+function autoIncrement() {
+	return slider.style.backgroundColor = colours[imageCount += 1];
+}
+
+function autoSlide() {
+	setInterval(changeSlider(1), 5000);
+}
+
+autoSlide();
