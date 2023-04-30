567573c88f905bd058b20346ee9652158a3f6b9a
Aiden Gibson <boerbasher@protonmail.com>
2023-04-30 14:18:22 +0100

    Todo: refactor code

--- module_playground_main.js
+++ module_playground_main.js
@@ -3,15 +3,24 @@ const colours = ['DarkCyan', 'DarkOrchid', 'DarkRed', 'DodgerBlue'];
 const rightButton = document.querySelector('button.right');
 const leftButton = document.querySelector('button.left');
 let imageCount = 0;
+let timeout;
+let timeMult = 1;
 slider.style.backgroundColor = colours[0];
 
-function changeImageCount(button) {
-	if (button === rightButton) {
-		return imageCount > colours.length-2 ? imageCount : imageCount += 1;
-	}
-	else {
-		return imageCount < 1 ? imageCount : imageCount -= 1;
-	}
+function changeBackground(number) {
+  return slider.style.backgroundColor = colour[number];
+}
+
+function increment(number, decrement=false) {
+  if (!decrement && imageCount + number === 5) {
+    return imageCount = 0;
+  }
+  else if (decrement && imageCount - number === -1) {
+    return imageCount = 4;
+  }
+  else {
+    return decrement ? imageCount -= number : imageCount += number;
+  }
 }
 
 function changeSlider(number) {
@@ -19,8 +28,7 @@ function changeSlider(number) {
 }
 
 rightButton.addEventListener('click', () => {
-	slider.style.backgroundColor = colours[changeImageCount(rightButton)];
-	console.log(imageCount);
+  changeBackground()
 }
 );
 
@@ -33,24 +41,11 @@ function autoIncrement() {
 	return slider.style.backgroundColor = colours[imageCount += 1];
 }
 
-function autoSlide() {
-	setInterval(changeSlider(1), 5000);
-}
-
-autoSlide();
-
-let timeout;
-let time = 1
-
 function myFunc(toAdd) {
-  timeout = setTimeout(myAlert, 5000 * toAdd);
-}
-
-function myAlert() {
-  console.log('Hello');
+  timeout = setTimeout(autoIncrement, 5000 * toAdd);
 }
 
 for (let t = 0; t < 10; t += 1) {
-  myFunc(time);
-  time += 1;
+  myFunc(timeMult);
+  timeMult += 1;
 }
